14ca01c0463fd946f5ac884a2dfde9fca24ac1fd
Viktor Poluksht <vpoluksht@gmail.com>
2022-11-07 04:38:40 +0000

    be62df1: protobuf update

---
+++ errors/errors.ts
@@ -0,0 +1,130 @@
+/* eslint-disable */
+import * as _m0 from "protobufjs/minimal";
+
+export const protobufPackage = "errors";
+
+export enum ErrorType {
+  Fatal = 0,
+}
+
+export function errorTypeFromJSON(object: any): ErrorType {
+  switch (object) {
+    case 0:
+    case "Fatal":
+      return ErrorType.Fatal;
+    default:
+      throw new globalThis.Error(
+        "Unrecognized enum value " + object + " for enum ErrorType"
+      );
+  }
+}
+
+export function errorTypeToJSON(object: ErrorType): string {
+  switch (object) {
+    case ErrorType.Fatal:
+      return "Fatal";
+    default:
+      throw new globalThis.Error(
+        "Unrecognized enum value " + object + " for enum ErrorType"
+      );
+  }
+}
+
+export interface Error {
+  type: ErrorType;
+}
+
+function createBaseError(): Error {
+  return { type: 0 };
+}
+
+export const Error = {
+  encode(message: Error, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
+    if (message.type !== 0) {
+      writer.uint32(8).int32(message.type);
+    }
+    return writer;
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): Error {
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = createBaseError();
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          message.type = reader.int32() as any;
+          break;
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(object: any): Error {
+    return {
+      type: isSet(object.type) ? errorTypeFromJSON(object.type) : 0,
+    };
+  },
+
+  toJSON(message: Error): unknown {
+    const obj: any = {};
+    message.type !== undefined && (obj.type = errorTypeToJSON(message.type));
+    return obj;
+  },
+
+  fromPartial<I extends Exact<DeepPartial<Error>, I>>(object: I): Error {
+    const message = createBaseError();
+    message.type = object.type ?? 0;
+    return message;
+  },
+};
+
+declare var self: any | undefined;
+declare var window: any | undefined;
+declare var global: any | undefined;
+var globalThis: any = (() => {
+  if (typeof globalThis !== "undefined") return globalThis;
+  if (typeof self !== "undefined") return self;
+  if (typeof window !== "undefined") return window;
+  if (typeof global !== "undefined") return global;
+  throw "Unable to locate global object";
+})();
+
+type Builtin =
+  | Date
+  | Function
+  | Uint8Array
+  | string
+  | number
+  | boolean
+  | undefined;
+
+export type DeepPartial<T> = T extends Builtin
+  ? T
+  : T extends Array<infer U>
+  ? Array<DeepPartial<U>>
+  : T extends ReadonlyArray<infer U>
+  ? ReadonlyArray<DeepPartial<U>>
+  : T extends { $case: string }
+  ? { [K in keyof Omit<T, "$case">]?: DeepPartial<T[K]> } & {
+      $case: T["$case"];
+    }
+  : T extends {}
+  ? { [K in keyof T]?: DeepPartial<T[K]> }
+  : Partial<T>;
+
+type KeysOfUnion<T> = T extends T ? keyof T : never;
+export type Exact<P, I extends P> = P extends Builtin
+  ? P
+  : P & { [K in keyof P]: Exact<P[K], I[K]> } & Record<
+        Exclude<keyof I, KeysOfUnion<P>>,
+        never
+      >;
+
+function isSet(value: any): boolean {
+  return value !== null && value !== undefined;
+}
